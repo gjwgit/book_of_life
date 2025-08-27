342959a881fd92566453e1c4fb68a133a9d757f2
Graham Williams <graham.williams@togaware.com>
2025-08-27 12:28:22 +1000

    Use wasm for web deployment

--- support/install.mk
+++ support/install.mk
@@ -2,7 +2,7 @@
 #
 # Makefile template for Installations
 #
-# Time-stamp: <Tuesday 2025-08-26 09:47:40 +1000 Graham Williams>
+# Time-stamp: <Tuesday 2025-08-26 12:32:11 +1000 Graham Williams>
 #
 # Copyright (c) Graham.Williams@togaware.com
 #
@@ -64,7 +64,7 @@ endif
 %.install:
 	cp web/index.html web/index.html.bak
 	perl -pi -e 's|^  <base href=.*$$|  <base href="/$*/">|' web/index.html
-	flutter build web
+	flutter build web --wasm
 	mv web/index.html.bak web/index.html
 	ssh solidcommunity.au 'if [ ! -e $(DEST:$(APP)=$*) ]; then echo mkdir /$(DEST:$(APP)=$*); fi'
 	rsync -azvh build/web/ solidcommunity.au:$(DEST:$(APP)=$*) --exclude *~ --exclude *.bak
