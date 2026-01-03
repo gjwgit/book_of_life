6d47196042bcf78863be5a5d4f473c3be544264d
Graham Williams <graham.williams@togaware.com>
2026-01-03 18:37:23 +1100

    Template updates.

--- installers/update.sh
+++ installers/update.sh
@@ -70,8 +70,6 @@ if [[ "${status}" == "completed" && "${conclusion}" == "success" ]]; then
 
     TARGET="${APP}_amd64.deb"
 
-    ## gh run download ${bumpId} --name ${APP}-linux-deb
-
     artifactId=$(gh api -H "Accept: application/vnd.github+json" /repos/${REP}/${APP}/actions/artifacts \
 		    --jq '.artifacts[] | select(.name | endswith("-linux-deb")) | .id' | head -n 1)
 
@@ -84,8 +82,9 @@ if [[ "${status}" == "completed" && "${conclusion}" == "success" ]]; then
 	fname=$(unzip -l artifact.zip | awk 'NR==4 {print $4}')
 	touch ${fname} # Timestamp with current date/time as the release time.
 	rm -f artifact.zip
-	echo  "Installing as ${DEST}${APP}_amd64.deb"
-	rsync -avzh ${fname} ${DEST}${APP}_amd64.deb
+	echo  "Installing as ${DEST}${TARGET}"
+	rsync -avzh ${fname} ${DEST}${TARGET}
+	ssh ${HOST} "cd ${FLDR}; chmod 0644 ${TARGET}"
 	echo  "Archive as installers/ARCHIVE/${fname}"
 	mv -f ${fname} ARCHIVE/
     fi
@@ -94,7 +93,7 @@ if [[ "${status}" == "completed" && "${conclusion}" == "success" ]]; then
 
     echo '******************** UPLOAD LINUX SNAP'
 
-    ## gh run download ${bumpId} --name ${APP}-linux-snap
+    TARGET="${APP}_amd64.snap"
 
     artifactId=$(gh api -H "Accept: application/vnd.github+json" /repos/${REP}/${APP}/actions/artifacts \
 		    --jq '.artifacts[] | select(.name | endswith("-linux-snap")) | .id' | head -n 1)
@@ -107,9 +106,9 @@ if [[ "${status}" == "completed" && "${conclusion}" == "success" ]]; then
 	fname=$(unzip -l artifact.zip | awk 'NR==4 {print $4}')
 	touch ${fname} # Timestamp with current date/time
 	rm -f artifact.zip
-	echo  "Installing as ${DEST}${APP}_amd64.snap"
-	rsync -avzh ${fname} ${DEST}${APP}_amd64.snap
-	ssh ${HOST} "cd ${FLDR}; chmod a+r ${APP}_amd64.snap"
+	echo  "Installing as ${DEST}${TARGET}"
+	rsync -avzh ${fname} ${DEST}${TARGET}
+	ssh ${HOST} "cd ${FLDR}; chmod 0644 ${TARGET}"
 	echo  "Archive as installers/ARCHIVE/${fname}"
 	mv -f ${fname} ARCHIVE/
     fi
@@ -118,7 +117,7 @@ if [[ "${status}" == "completed" && "${conclusion}" == "success" ]]; then
 
     echo '******************** UPLOAD LINUX ZIP'
 
-    ## gh run download ${bumpId} --name ${APP}-linux-zip
+    TARGET="${APP}-linux.zip"
 
     artifactId=$(gh api -H "Accept: application/vnd.github+json" /repos/${REP}/${APP}/actions/artifacts \
 		    --jq '.artifacts[] | select(.name | endswith("-linux-zip")) | .id' | head -n 1)
@@ -132,8 +131,9 @@ if [[ "${status}" == "completed" && "${conclusion}" == "success" ]]; then
 	fname=$(unzip -l artifact.zip | awk 'NR==4 {print $4}')
 	touch ${fname} # Timestamp with current date/time
 	rm -f artifact.zip
-	echo  "Installing as ${DEST}${APP}-linux.zip"
-	rsync -avzh ${fname} ${DEST}${APP}-linux.zip
+	echo  "Installing as ${DEST}${TARGET}"
+	rsync -avzh ${fname} ${DEST}${TARGET}
+	ssh ${HOST} "cd ${FLDR}; chmod 0644 ${TARGET}"
 	echo  "Archive as installers/ARCHIVE/${APP}_${version}_linux.zip"
 	mv -f ${fname} ARCHIVE/${APP}_${version}_linux.zip
     fi
@@ -141,8 +141,6 @@ if [[ "${status}" == "completed" && "${conclusion}" == "success" ]]; then
     echo ""
 
     echo '******************** UPLOAD MACOS ZIP ORIGINAL'
-
-    ## gh run download ${bumpId} --name ${APP}-macos-zip
 
     artifactId=$(gh api -H "Accept: application/vnd.github+json" /repos/${REP}/${APP}/actions/artifacts \
 		    --jq '.artifacts[] | select(.name | endswith("-macos-zip")) | .id' | head -n 1)
@@ -158,7 +156,7 @@ if [[ "${status}" == "completed" && "${conclusion}" == "success" ]]; then
 	rm -f artifact.zip
 	echo  "Installing as ${DEST}${APP}-macos.zip"
 	rsync -avzh ${APP}-macos.zip ${DEST}
-	ssh ${HOST} "cd ${FLDR}; chmod a+r ${APP}-*.zip ${APP}-*.exe"
+	ssh ${HOST} "cd ${FLDR}; chmod 0644 ${APP}-macos.zip"
 	echo  "Archive as installers/ARCHIVE/${APP}_${version}_macos.zip"
 	mv ${APP}-macos.zip ARCHIVE/${APP}_${version}_macos.zip
     fi
@@ -166,8 +164,6 @@ if [[ "${status}" == "completed" && "${conclusion}" == "success" ]]; then
     echo ""
 
     echo '******************** UPLOAD MACOS DMG ORIGINAL'
-
-    ## gh run download ${bumpId} --name ${APP}-macos-zip
 
     artifactId=$(gh api -H "Accept: application/vnd.github+json" /repos/${REP}/${APP}/actions/artifacts \
 		    --jq '.artifacts[] | select(.name | endswith("-macos-dmg")) | .id' | head -n 1)
@@ -185,7 +181,7 @@ if [[ "${status}" == "completed" && "${conclusion}" == "success" ]]; then
 	rm -f artifact.zip
 	echo  "Installing as ${DEST}${fname}"
 	rsync -avzh ${fname} ${DEST}
-	ssh ${HOST} "cd ${FLDR}; chmod a+r ${fname}"
+	ssh ${HOST} "cd ${FLDR}; chmod 0644 ${fname}"
 	echo  "Archive as installers/ARCHIVE/${APP}_${version}_macos.dmg"
 	mv ${fname} ARCHIVE/${APP}_${version}_macos.dmg
     fi
@@ -214,7 +210,7 @@ if [[ "${status}" == "completed" && "${conclusion}" == "success" ]]; then
 	rm -f artifact.zip
 	echo  "Installing as ${DEST}${fname}"
 	rsync -avzh ${fname} ${DEST}
-	ssh ${HOST} "cd ${FLDR}; chmod a+r ${fname}"
+	ssh ${HOST} "cd ${FLDR}; chmod 0644 ${fname}"
 	echo  "Archive as installers/ARCHIVE/${APP}_${version}_macos_unsigned.dmg"
 	mv ${fname} ARCHIVE/${APP}_${version}_macos_unsigned.zip
     fi
@@ -237,7 +233,7 @@ if [[ "${status}" == "completed" && "${conclusion}" == "success" ]]; then
 	rm -f artifact.zip
 	echo  "Installing as ${DEST}${fname}"
 	rsync -avzh ${fname} ${DEST}
-	ssh ${HOST} "cd ${FLDR}; chmod a+r ${fname}"
+	ssh ${HOST} "cd ${FLDR}; chmod 0644 ${fname}"
 	echo  "Archive as installers/ARCHIVE/${APP}_${version}_macos_unsigned.dmg"
 	mv ${fname} ARCHIVE/${APP}_${version}_macos_unsigned.dmg
     fi
@@ -245,8 +241,6 @@ if [[ "${status}" == "completed" && "${conclusion}" == "success" ]]; then
     echo ""
 
     echo '******************** UPLOAD MACOS DMG STAGING'
-
-    ## gh run download ${bumpId} --name ${APP}-macos-zip
 
     artifactId=$(gh api -H "Accept: application/vnd.github+json" /repos/${REP}/${APP}/actions/artifacts \
 		    --jq '.artifacts[] | select(.name | endswith("-macos-staging-dmg")) | .id' | head -n 1)
@@ -262,7 +256,7 @@ if [[ "${status}" == "completed" && "${conclusion}" == "success" ]]; then
 	rm -f artifact.zip
 	echo  "Installing as ${DEST}${fname}"
 	rsync -avzh ${fname} ${DEST}
-	ssh ${HOST} "cd ${FLDR}; chmod a+r ${APP}-dev-macos-staging.dmg"
+	ssh ${HOST} "cd ${FLDR}; chmod 0644 ${fname}"
 	echo  "Archive as installers/ARCHIVE/${APP}_${version}_macos_staging.dmg"
 	mv ${fname} ARCHIVE/${APP}_${version}_macos_staging.dmg
     fi
@@ -270,8 +264,6 @@ if [[ "${status}" == "completed" && "${conclusion}" == "success" ]]; then
     echo ""
 
     echo '******************** UPLOAD MACOS DMG DEV'
-
-    ## gh run download ${bumpId} --name ${APP}-macos-zip
 
     artifactId=$(gh api -H "Accept: application/vnd.github+json" /repos/${REP}/${APP}/actions/artifacts \
 		    --jq '.artifacts[] | select(.name | endswith("-macos-dev-dmg")) | .id' | head -n 1)
@@ -287,7 +279,7 @@ if [[ "${status}" == "completed" && "${conclusion}" == "success" ]]; then
 	rm -f artifact.zip
 	echo  "Installing as ${DEST}${fname}"
 	rsync -avzh ${fname} ${DEST}
-	ssh ${HOST} "cd ${FLDR}; chmod a+r ${fname}"
+	ssh ${HOST} "cd ${FLDR}; chmod 0644 ${fname}"
 	echo  "Archive as installers/ARCHIVE/${APP}_${version}_macos_dev.dmg"
 	mv ${fname} ARCHIVE/${APP}_${version}_macos_dev.dmg
     fi
@@ -295,8 +287,6 @@ if [[ "${status}" == "completed" && "${conclusion}" == "success" ]]; then
     echo ""
 
     echo '******************** UPLOAD WINDOWS INNO'
-
-    ## gh run download ${bumpId} --name ${APP}-windows-inno
 
     artifactId=$(gh api -H "Accept: application/vnd.github+json" /repos/${REP}/${APP}/actions/artifacts \
 		    --jq '.artifacts[] | select(.name | endswith("-windows-inno")) | .id' | head -n 1)
@@ -312,6 +302,7 @@ if [[ "${status}" == "completed" && "${conclusion}" == "success" ]]; then
 	rm -f artifact.zip
 	echo  "Installing as ${DEST}${fname}"
 	rsync -avzh ${fname} ${DEST}
+	ssh ${HOST} "cd ${FLDR}; chmod 0644 ${fname}"
 	echo  "Archive as installers/ARCHIVE/${APP}_${version}_windows_inno.exe"
 	mv ${fname} ARCHIVE/${APP}_${version}_windows_inno.exe
     fi
@@ -319,8 +310,6 @@ if [[ "${status}" == "completed" && "${conclusion}" == "success" ]]; then
     echo ""
 
     echo '******************** UPLOAD WINDOWS ZIP'
-
-    ## gh run download ${bumpId} --name ${APP}-windows-zip
 
     artifactId=$(gh api -H "Accept: application/vnd.github+json" /repos/${REP}/${APP}/actions/artifacts \
 		    --jq '.artifacts[] | select(.name | endswith("-windows-zip")) | .id' | head -n 1)
@@ -336,10 +325,12 @@ if [[ "${status}" == "completed" && "${conclusion}" == "success" ]]; then
 	rm -f artifact.zip
 	echo  "Installing as ${DEST}${fname}"
 	rsync -avzh ${fname} ${DEST}
-	ssh ${HOST} "cd ${FLDR}; chmod a+r ${APP}-*.zip ${APP}-*.exe"
+	ssh ${HOST} "cd ${FLDR}; chmod 0644 ${fname}"
 	echo  "Archive as installers/ARCHIVE/${APP}_${version}_windows.zip"
 	mv -f ${APP}-windows.zip ARCHIVE/${APP}_${version}_windows.zip
     fi
+
+    echo ""
 
 else
 
